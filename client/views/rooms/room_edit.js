f57e105a55338e56fc6898d6ffa81a4824c60392
lisandro_linux <lisandro@ubuntu.(none)>
2014-08-30 22:33:15 -0300

    confnodized files, advance room creation, fix issues

---
+++ client/views/rooms/room_edit.js
@@ -0,0 +1,50 @@
+Template.roomEdit.events({
+  'submit form': function(e) {
+    e.preventDefault();
+    
+    var currentRoomId = this._id;
+    
+    var roomProperties = {
+      url: $(e.target).find('[name=url]').val(),
+      title: $(e.target).find('[name=title]').val()
+    }
+    
+    Rooms.update(currentRoomId, {$set: roomProperties}, function(error) {
+      if (error) {
+        // display the error to the user
+        alert(error.reason);
+      } else {
+        Router.go('roomPage', {_id: currentRoomId});
+      }
+    });
+  },
+  
+  'click .delete': function(e) {
+    e.preventDefault();
+    
+    if (confirm("Delete this room?")) {
+      var currentRoomId = this._id;
+      Rooms.remove(currentRoomId);
+      Router.go('rommsList');
+    }
+  }
+});
+
+Template.roomEdit.rendered = function () {
+  var my_custom_options = {
+    "no-duplicate": true,
+    "no-duplicate-callback": window.alert,
+    "no-duplicate-text": "Duplicate tags",
+    "type-zone-class": "type-zone",
+    "tag-box-class": "tagging",
+    "forbidden-chars": [",", "?"]
+  };
+  var t = $("#roomtagsEdit").tagging(my_custom_options);
+  t[0].addClass("form-control");
+  
+  var t1 = $("#invitesEdit").tagging(my_custom_options);
+  t1[0].addClass("form-control");
+
+  $('#datetimepicker1').datetimepicker();
+
+}
---
+++ client/views/rooms/room_submit.js
@@ -0,0 +1,52 @@
+Template.roomSubmit.events({
+  'submit form': function(e) {
+    e.preventDefault();
+    
+      var t, $tag_box;
+      // We call taggingJS init on all "#tag" divs
+      //t = $( "#roomtags" ).tagging();
+
+      // This is the $tag_box object of the first captured div
+      //$tag_box = t[0];
+
+      // To get all tags inside tag box as an array of String
+      //$tag_box.tagging( "getTags" );
+
+    var room = {
+      name: $(e.target).find('[id=name]').val(),
+      description: $(e.target).find('[id=desc]').val(),
+      tags: $(e.target).find('[id=desc]').val(),
+      guests: $(e.target).find('[id=desc]').val(),
+      privacy: $(e.target).find('[name=my-checkbox0]').val(),
+      scheduled: $(e.target).find('[name=my-checkbox]').val(),
+      datetime: $(e.target).find('[id=datetimepicker1]').val()
+    }
+    
+    Meteor.call('room', room, function(error, id) {
+      if (error)
+        return alert(error.reason);
+
+      Router.go('roomPage', {_id: id});
+    });
+  }
+});
+
+Template.roomSubmit.rendered = function () {
+  var my_custom_options = {
+    "no-duplicate": true,
+    "no-duplicate-callback": window.alert,
+    "no-duplicate-text": "Duplicate tags",
+    "type-zone-class": "type-zone",
+    "tag-box-class": "tagging",
+    "forbidden-chars": [",", "?"]
+  };
+  var t = $("#roomtags").tagging(my_custom_options);
+  t[0].addClass("form-control");
+  
+  var t1 = $("#invites").tagging(my_custom_options);
+  t1[0].addClass("form-control");
+
+  $('.make-switch').bootstrapSwitch();
+  $('#datetimepicker1').datetimepicker();
+
+}
