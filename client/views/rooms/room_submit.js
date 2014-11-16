97bd48152113863fcfd9389082d551416ead359d
Emiliano Lippolis <emilipo@gmail.com>
2014-11-16 19:25:46 -0300

    Various changes in room_submit
    Change tagit containers to use name attribute instead of id.
    Dinamically disable accessPassword and scheduledTime inputs.
    Use validation constraints in datetimepicker.
    Some fields renames.

--- client/views/rooms/room_submit.js
+++ client/views/rooms/room_submit.js
@@ -7,21 +7,35 @@ Template.roomSubmit.events({
         var room = {
             name: $form.find('[name="name"]').val(),
             description: $form.find('[name="description"]').val(),
-            privacy: $form.find('[name="public"]').val()? 'public':'private',
-            accessPin: $form.find('[name="accessPin"]').val(),
+            tags: $form.find('[name="tags"]').tagit("assignedTags"),
+            guests: $form.find('[name="guests"]').tagit("assignedTags"),
+            public: $form.find('[name="public"]').prop('checked'),
+            accessPassword: $form.find('[name="accessPassword"]').val(),
             scheduled: $form.find('[name="scheduled"]').prop('checked'),
-            schedulingTime: $form.find('[name="schedulingTime"]').data("DateTimePicker").getDate(),
+            scheduledTime: new Date($form.find('[name="scheduledTime"]')
+                                         .data("DateTimePicker").getDate()),
             chat: $form.find('[name="chat"]').prop('checked')
-        }
-            tags: $form.find('#tags').tagit("assignedTags"),
-            guests: $form.find('#guests').tagit("assignedTags"),
+        };
 
         Meteor.call('roomInsert', room, function(error, result) {
-            if (error)
-                return alert(error.reason);
-
+            if (error) {
+                return alert(error.message);
+            }
             Router.go('roomPage', {_id: result._id});
         });
+    },
+
+    'switchChange.bootstrapSwitch #form_submit [name="public"]': function() {
+        $('#form_submit [name="accessPassword"]').prop('disabled', $('#form_submit [name="public"]').prop('checked'));
+    },
+
+    'switchChange.bootstrapSwitch #form_submit [name="scheduled"]': function() {
+        var dateTimePicker = $('#form_submit [name="scheduledTime"]').data("DateTimePicker")
+        if ($('#form_submit [name="scheduled"]').prop('checked')) {
+            dateTimePicker.enable();
+        } else {
+            dateTimePicker.disable();
+        }
     }
 });
 
@@ -30,11 +44,14 @@ Template.roomSubmit.rendered = function() {
         'removeConfirmation': true,
         'caseSensitive': false
     };
-    $('#tags').tagit(tagitOptions);
-    $('#guests').tagit(tagitOptions);
-    
+    $('#form_submit [name="tags"]').tagit(tagitOptions);
+    $('#form_submit [name="guests"]').tagit(tagitOptions);
+
+    var date = new Date();
+    $('#form_submit [name="scheduledTime"]').datetimepicker({
+        minDate: date,
+        defaultDate: date
     });
 
-    $('.datetimepicker').datetimepicker();
     $('.bootstrap-switch').bootstrapSwitch();
 };
--- collections/rooms.js
+++ collections/rooms.js
@@ -1,17 +1,3 @@
-/*
- name
- description
- tags
- guests (mail invites)
- privacy (public or private)
- when (now or scheduled) and (datetime scheduled or without date restriction: now, in ten minutes,... )
- creator
- submitted date
-
- plugins enabled?
-
- */
-
 Rooms = new Meteor.Collection('rooms');
 
 Rooms.allow({
@@ -22,38 +8,50 @@ Rooms.allow({
 Rooms.deny({
     update: function(userId, room, fieldNames) {
         // may only edit the following fields:
-        return (_.without(fieldNames, 'guests', 'description', 'privacy', 'date', 'name', 'tags', 'scheduled', 'datetime', 'chat').length > 0);
+        return (_.without(fieldNames, 'name', 'description', 'tags', 'guests', 'public', 'accessPassword', 'scheduled', 'scheduledTime', 'chat').length > 0);
     }
 });
 
 Meteor.methods({
     roomInsert: function(room) {
         check(Meteor.userId(), String);
-        console.log(room.schedulingTime);
         check(room, {
             name: String,
             description: String,
             tags: Array,
             guests: Array,
-            privacy: Match.OneOf('public', 'private'),
-            accessPin: String,
+            public: Boolean,
+            accessPassword: String,
             scheduled: Boolean,
             // See how the fuck match a Date here, tried but Meteor has decided to kill a kitten instead
-            schedulingTime: Match.Any,
+            scheduledTime: Date, //Deal with it. (•_•) / ( •_•)>⌐■-■  / (⌐■_■)
             chat: Boolean
         });
 
-        if (!room.scheduled)
-            room.schedulingTime = null;
+        //Room Name cannot be empty
+        if (!room.name) {
+            throw new Meteor.Error(422, 'Please, fill in the room name');
+        }
 
-        if (!room.name)
-            throw new Meteor.Error(422, 'Please fill the room name');
+        //If Room is not Public then it has to have an Access Password
+        if (!room.public && !room.accessPassword) {
+            throw new Meteor.Error(422, 'Please, provide an access password for private room');
+        }
+
+        //If Room is not Scheduled then no need for a Time
+        if (!room.scheduled) {
+            room.scheduledTime = null;
+        }
+        //If the Room IS Scheduled for a Time, then it cannot be older than right now
+        else if (compareDates(room.scheduledTime, new Date()) < 0) {
+            throw new Meteor.Error(422, 'The scheduled date must be in the future!!!!11 Are you a time traveller?');
+        }
 
         var user = Meteor.user();
         var room = _.extend(room, {
             userId: user._id,
             creator: user.username,
-            submitted: new Date()
+            submittedTime: new Date()
         });
 
         var roomId = Rooms.insert(room);
@@ -63,3 +61,21 @@ Meteor.methods({
         };
     }
 });
+
+/**
+ * Compares two dates
+ * @param {Date} d1 first date
+ * @param {Date} d2 second date
+ * @returns {integer} =0 if dates are equal, <0 if first date is earlier than second one
+ */
+function compareDates(d1, d2) {
+    if (d1.getYear() != d2.getYear())
+        return d1.getYear() - d2.getYear();
+    if (d1.getMonth() != d2.getMonth())
+        return d1.getMonth() - d2.getMonth();
+    if (d1.getDate() != d2.getDate())
+        return d1.getDate() - d2.getDate();
+    if (d1.getHours() != d2.getHours())
+        return d1.getHours() - d2.getHours();
+    return d1.getMinutes() - d2.getMinutes()
+}
