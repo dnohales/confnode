86da8e4ea4077a39e3d5e8b2be09c08e5902b611
Emiliano Lippolis <emilipo@gmail.com>
2014-11-16 19:34:21 -0300

    Implements delete button functionallity in room list

--- client/views/rooms/room_item.js
+++ client/views/rooms/room_item.js
@@ -2,9 +2,19 @@ Template.roomItem.helpers({
     ownRoom: function() {
         return this.userId == Meteor.userId();
     },
+
     domain: function() {
         var a = document.createElement('a');
         a.href = this.url;
         return a.hostname;
+    }
+});
+
+Template.roomsList.events({
+    'click .delete-room-button': function(e) {
+        e.preventDefault();
+        if (confirm("Are you sure you want to delete this room?\nRoom name: " + this.name)) {
+            Rooms.remove(this._id);
+        }
     }
 });
