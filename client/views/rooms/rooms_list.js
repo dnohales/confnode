44ae12da011ea449c83f31e7b8bb627836746c0f
Damián Nohales <damiannohales@gmail.com>
2015-01-03 15:14:41 -0300

    Implement basic search

--- client/views/rooms/rooms_list.js
+++ client/views/rooms/rooms_list.js
@@ -1,22 +1,50 @@
 Template.roomsList.helpers({
     rooms: function() {
     	var user = Meteor.user();
-        return Rooms.find({
-            $or: [{
-                listed: true
-            }, {
-                presenter: {
-                    $in: [user.emails[0].address]
+        var query = new RegExp(Session.get('search-query'), 'i');
+        console.log(query);
+
+        return Rooms.find(
+            {
+                $and: [
+                    {
+                        $or: [{
+                            name: query
+                        }, {
+                            description: query
+                        }, {
+                            tags: {
+                                $in: [query]
+                            }
+                        }],
+                    },
+                    {
+                        $or: [{
+                            listed: true
+                        }, {
+                            presenter: {
+                                $in: [user.emails[0].address]
+                            }
+                        }, {
+                            guests: {
+                                $in: [user.emails[0].address]
+                            }
+                        }]
+                    }
+                ]
+            },
+            {
+                sort: {
+                    submitted: -1
                 }
-            }, {
-                guests: {
-                    $in: [user.emails[0].address]
-                }
-            }]
-        }, {
-            sort: {
-                submitted: -1
             }
-        });
+        );
+    }
+});
+
+Template.roomsList.events({
+    'keyup #search_form [name="query"]': function(e) {
+        var query = $(e.target).val();
+        Session.set('search-query', $.trim(query));
     }
 });
